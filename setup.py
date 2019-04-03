468e4e75cfd63a52427ab9f0935931e2ebbc12f3
Sunil.patel <sunil.patel@innoplexus.com>
2019-04-03 20:07:37 +0530

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import setuptools
+__author__ = 'sunil.patel <innoplexus.com>'
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -17,9 +19,31 @@ setuptools.setup(
       'myapp': ['extensible_classification_framework/src/utils/resources/ontology_for_tokenizer.list'],
    },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Text Editors :: Text Processing',
+    ],
+    install_requires=[
+        'nltk==3.2.4'
+        'chakin==0.0.6'
+        'torch==0.4.1'
+        'tqdm==4.31.1'
+        'torchtext==0.3.1'
+        'matplotlib==2.2.2'
+        'pandas==0.24.1'
+        'gensim==3.7.1'
+        'numpy==1.16.1'
+
     ],
 
 )
